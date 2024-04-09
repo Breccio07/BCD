ed1e43913c7fac046d2b8dda366384459e520384
Daniele Morelli <Daniele Morelli>
2024-04-09 11:30:37 +0200

    finita verifica

--- BCD.java
+++ BCD.java
@@ -38,10 +38,34 @@ public class BCD extends Application{
 		griglia.add(lBCD, 0, 3);
 		griglia.add(lRisultato, 0, 4);
 		
-		
+		pConverti.setOnAction(e -> Converti());
+
 	}
 
-	
+		private void Converti() {
+		
+		String vS[]= tNumero.getText().split(""), BCD="";
+		
+		Hashtable <String,String> converti =  new Hashtable <String,String>();
+		
+		converti.put("0", "0000");
+		converti.put("1", "0001");
+		converti.put("2", "0010");
+		converti.put("3", "0011");
+		converti.put("4", "0100");
+		converti.put("5", "0101");
+		converti.put("6", "0110");
+		converti.put("7", "0111");
+		converti.put("8", "1000");
+		converti.put("9", "1001");
+		
+		for(int i=0; i < vS.length; i++) {
+			
+			BCD += converti.get(vS[i]);			
+		}
+		lRisultato.setText(BCD);
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
